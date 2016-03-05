5fcc669bfb56d8fd389f287b6cd64f3e9fffcd5b
travis kirton <travis@c4ios.com>
2016-03-04 21:31:02 -0800

    Initial Commit

---
+++ Cosmos/WorkSpace.swift
@@ -0,0 +1,19 @@
+//
+//  WorkSpace.swift
+//  Cosmos
+//
+//  Created by travis on 2016-03-04.
+//  Copyright © 2016 C4. All rights reserved.
+//
+
+import UIKit
+
+class WorkSpace: CanvasController {
+
+    override func setup() {
+        //Work your magic here.
+    }
+
+
+}
+
